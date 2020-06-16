416f1b93c40e73bfbfbc66091a67d3b7d92d162e
Ninja <samir932205@gmail.com>
2020-06-17 00:56:10 +0200

    basic
    d

--- server/main.lua
+++ server/main.lua
@@ -1,11 +1,11 @@
 --||@SuperCoolNinja.||--
 
 --> Version de la Resource : 
-local LatestVersion = ''; CurrentVersion = '1.1'
+local LatestVersion = ''; CurrentVersion = '1.0'
 PerformHttpRequest('https://raw.githubusercontent.com/NinjaSourceV2/GTA_MenuAdmin/master/VERSION', function(Error, NewestVersion, Header)
     LatestVersion = NewestVersion
     if CurrentVersion ~= NewestVersion then
-        print("\n\r ^2[GTA_Superette]^1 La version que vous utilisé n'est plus a jours, veuillez télécharger la dernière version. ^3\n\r")
+        print("\n\r ^2[GTA_MenuAdmin]^1 La version que vous utilisé n'est plus a jours, veuillez télécharger la dernière version. ^3\n\r")
     end
 end)
 
